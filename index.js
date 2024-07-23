42a09c48415bf3e350522ec040045df1a2ca90da
Tmob <mrrtmob@gmail.com>
2024-07-23 14:04:02 +0700

    gg

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ export const config = {
     process.env.DATABASE_URI || process.env.MONGODB_URI || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || path.join(__dirname, '/cloud/main.js'),
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || 'myClientId', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'https://parse-dev.evalley.io/parse', // Don't forget to change to https if needed
+  masterKey: process.env.MASTER_KEY || 'masterKey', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
   allowClientClassCreation: true,
   liveQuery: {
     classNames: ['Message', 'Room']
